66aefa2d0d63022231ca8d5a99450b0bc595700e
Luis Davim <luis.davim@smartbox.com>
2016-11-24 23:03:31 +0000

    adding list of PPAs and repos

---
+++ restore-ppas.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-add-repository ppa:alexlarsson/flatpak
+sudo apt-add-repository ppa:snwh/pulp
+sudo apt-add-repository ppa:webupd8team/atom
+sudo apt-add-repository ppa:fish-shell/release-2
+sudo apt-add-repository ppa:atareao/atareao
+sudo apt-add-repository ppa:linrunner/tlp
+sudo apt-add-repository ppa:webupd8team/y-ppa-manager
+sudo apt-add-repository ppa:libreoffice/ppa
+sudo apt-add-repository ppa:team-xbmc/ppa
+sudo apt-add-repository ppa:neovim-ppa/unstable
+sudo apt-add-repository ppa:fixnix/indicator-systemtray-unity
+sudo apt-add-repository ppa:fingerprint/fingerprint-gui
+sudo apt-add-repository ppa:webupd8team/terminix
+sudo apt-add-repository ppa:ultradvorka/ppa
+sudo apt-add-repository ppa:nilarimogard/webupd8
+sudo apt-add-repository ppa:pi-rho/dev
