7deb541a1206380c27489717f12066158faadf13
Luis Davim <luis.davim@smartbox.com>
2016-12-14 18:08:55 +0000

    set terminal font WIP

--- install.sh
+++ install.sh
@@ -87,6 +87,9 @@ function installFonts() {
 }
 
 function installGnomeTerminalProfiles() {
+   # gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Ubuntu Mono derivative Powerline 11"
+   # gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type=boolean false
+
   # Profiles from https://github.com/Mayccoll/Gogh
   export {PROFILE_NAME,PROFILE_SLUG}="OneDark" && wget -O xt http://git.io/vs7Ut && chmod +x xt && ./xt && rm xt
   export {PROFILE_NAME,PROFILE_SLUG}="MonokaiDark" && wget -O xt http://git.io/v3DBO && chmod +x xt && ./xt && rm xt
