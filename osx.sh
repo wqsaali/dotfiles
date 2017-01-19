b4117b726db695fac057a13c3df416cc4381b0ea
Luis Davim <ldavim@ptc.com>
2017-01-19 10:15:38 +0000

    OS X keyboard speed setup

--- osx.sh
+++ osx.sh
@@ -82,7 +82,8 @@ osConfigs() {
   # Enable Key Repeat
   defaults write -g ApplePressAndHoldEnabled -bool false
   # Set keyboard repeate speed
-  defaults write -g KeyRepeat -int 0.02
+  defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+  defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
   # Dock
   # Add a Stack with Recent Applications
