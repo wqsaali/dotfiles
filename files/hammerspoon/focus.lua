54bddf11c9227d8a337303ebf86b66807f733737
Luis Davim <ldavim@ptc.com>
2017-03-28 18:15:15 +0100

    updating hammerspoon config

---
+++ files/hammerspoon/focus.lua
@@ -0,0 +1,35 @@
+-----------------------------------------------
+-- Hyper ctlr up,down,left,right to switch window focus
+-----------------------------------------------
+
+hs.hotkey.bind(hyperCtlr, 'up', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowNorth()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyperCtlr, 'down', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowSouth()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyperCtlr, 'right', function()
+    if hs.window.focusedWindow() then
+    hs.window.focusedWindow():focusWindowEast()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyperCtlr, 'left', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowWest()
+    else
+        hs.alert.show("No active window")
+    end
+end)
