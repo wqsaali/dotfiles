89b4e2a0387f7f51af360e146fa96b7fe40cd0ca
Luis Davim <ldavim@ptc.com>
2017-03-29 12:39:47 +0100

    updating hammerspoon config

--- files/hammerspoon/init.lua
+++ files/hammerspoon/init.lua
@@ -15,3 +15,11 @@ require('watcher')
 require('position')
 require('focus')
 
+-----------------------------------------------
+-- Lock
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'l', function()
+  -- os.execute("open '/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app'")
+  os.execute("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend")
+end)
