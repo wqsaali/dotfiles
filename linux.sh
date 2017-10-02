f628abe31e4ae547da0237ba4e9d08f5273a076f
Luis Davim <ldavim@ptc.com>
2017-10-02 20:31:00 +0100

    [WIP] changes for ubuntu 17.10

--- linux.sh
+++ linux.sh
@@ -141,6 +141,7 @@ addExtraRepos() {
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
   echo 'deb http://repository.spotify.com stable non-free' | sudo tee /etc/apt/sources.list.d/spotify.list
   # Skype
+  wget -q -O - https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
   echo 'deb [arch=amd64] https://repo.skype.com/deb stable main' | sudo tee /etc/apt/sources.list.d/skype-stable.list
 }
 
