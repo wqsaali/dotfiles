df1428409849c352efbcc9a63c8c0ab143c879a1
Luis Davim <ldavim@ptc.com>
2018-10-31 23:10:59 +0000

    updated prompt theme

--- files/bash/git-prompt-colors.sh
+++ files/bash/git-prompt-colors.sh
@@ -51,10 +51,10 @@ override_git_prompt_colors() {
   # GIT_PROMPT_UPSTREAM=" {${Blue}_UPSTREAM_${ResetColor}}"
 
   ## _LAST_COMMAND_INDICATOR_ will be replaced by the appropriate GIT_PROMPT_COMMAND_OK OR GIT_PROMPT_COMMAND_FAIL
-  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${Cyan}${PathShort}${ResetColor} \$(kube_ps1)"
-  GIT_PROMPT_START_ROOT="_LAST_COMMAND_INDICATOR_ ${Red}${PathShort}${ResetColor} \$(kube_ps1)"
-  GIT_PROMPT_END_USER=" \n${DimWhite}${Time12a}${ResetColor} $ "
-  GIT_PROMPT_END_ROOT=" \n${DimWhite}${Time12a}${ResetColor} # "
+  GIT_PROMPT_START_USER="┌_LAST_COMMAND_INDICATOR_ ${Cyan}${PathShort}${ResetColor} \$(kube_ps1)"
+  GIT_PROMPT_START_ROOT="┌_LAST_COMMAND_INDICATOR_ ${Red}${PathShort}${ResetColor} \$(kube_ps1)"
+  GIT_PROMPT_END_USER=" \n└ ${DimWhite}${Time12a}${ResetColor} $ "
+  GIT_PROMPT_END_ROOT=" \n└ ${DimWhite}${Time12a}${ResetColor} # "
 
   ## Please do not add colors to these symbols
   # GIT_PROMPT_SYMBOLS_AHEAD="↑·"             # The symbol for "n versions ahead of origin"
