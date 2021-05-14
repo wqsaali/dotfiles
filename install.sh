c955ea7514f9f523e27120ee930c50154cabc1fe
Luis Davim <luis.davim@jet.com>
2021-05-14 16:39:24 +0100

    install ytt using brew

--- install.sh
+++ install.sh
@@ -141,7 +141,6 @@ installKubeScripts() {
   installFromGithub 'kubernetes-sigs/kubebuilder' "${1}" "${2}"
   # installFromGithub 'operator-framework/operator-sdk' "${1}" "${2}"
   installFromGithub 'nutellinoit/kubenvz' "${1}" "${2}"
-  installFromGithub 'k14s/ytt' "${1}" "${2}"
   installFromGithub 'jaredallard/localizer' "${1}" "${2}"
 
   if [ ! -s ${HOME}/.local/bin/kuberlr ]; then
